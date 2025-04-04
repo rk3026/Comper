0a29c3d0427c1515ff84289fe087e7364ac62fae
Ross <rosskugler@gmail.com>
2025-04-04 14:37:45 -0700

    working on connecting front and backend

---
+++ setup.bat
@@ -0,0 +1,19 @@
+@echo off
+echo ================================
+echo Installing frontend dependencies
+echo ================================
+cd privacy-forum-frontend
+call npm install
+cd ..
+
+echo ================================
+echo Installing backend dependencies
+echo ================================
+cd privacy-forum-backend
+call npm install cors express body-parser sqlite3
+cd ..
+
+echo ================================
+echo ✅ All dependencies installed!
+echo ================================
+pause
