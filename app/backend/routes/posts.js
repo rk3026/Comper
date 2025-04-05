b7a14c1e805eb09f16b37495467aaff60334d6dc
Ross <rosskugler@gmail.com>
2025-04-04 17:09:25 -0700

    working on database connection

--- app/backend/routes/posts.js
+++ app/backend/routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { pool } = require('../db/database');  // Correct path to database.js
+const { pool } = require('../db/database');
 
 // Get all posts
 router.get('/', async (req, res) => {
